24aff7bd349387cd6c71b18756ba0920106346f0
shareef0911 <shaikbajishareef0911@gmail.com>
2026-04-06 09:45:56 +0530

    fiveth commit

---
+++ src/com/languageFun/TypeCasting.java
@@ -0,0 +1,24 @@
+package com.languageFun;
+
+
+import java.util.Scanner;
+
+public class TypeCasting {
+	
+	double d1 = 9087654321.546876543;
+	
+	int i1 = (int) d1;
+	float f1 = (float) i1;
+	long l1 = (long) f1;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TypeCasting t1 = new TypeCasting();
+		System.out.println("Double no : "+t1.d1);
+		System.out.println("Int No : "+t1.i1);
+		System.out.println("Float No : "+t1.f1);
+		System.out.println("Long No : "+t1.l1);
+	}
+
+}
